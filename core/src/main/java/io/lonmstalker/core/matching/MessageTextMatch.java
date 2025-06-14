82279f1cdf151b80f7eb2a19fcd896937ea44cb5
Nikita <95490604+lonmstalker@users.noreply.github.com>
2025-06-14 17:13:37 +0300

    feat(core): add default CommandMatch implementations

---
+++ core/src/main/java/io/lonmstalker/core/matching/MessageTextMatch.java
@@ -0,0 +1,33 @@
+package io.lonmstalker.core.matching;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.telegram.telegrambots.meta.api.objects.Message;
+
+/**
+ * Matcher that checks message text equality.
+ */
+public class MessageTextMatch implements CommandMatch<Message> {
+
+    private final String text;
+    private final boolean ignoreCase;
+
+    public MessageTextMatch(@NonNull String text) {
+        this(text, false);
+    }
+
+    public MessageTextMatch(@NonNull String text, boolean ignoreCase) {
+        this.text = text;
+        this.ignoreCase = ignoreCase;
+    }
+
+    @Override
+    public boolean match(@NonNull Message data) {
+        if (data.getText() == null) {
+            return false;
+        }
+        if (ignoreCase) {
+            return text.equalsIgnoreCase(data.getText());
+        }
+        return text.equals(data.getText());
+    }
+}
