302a89b8814f9d4b89a1fc877e3d8d1156cff86f
Nikita <95490604+lonmstalker@users.noreply.github.com>
2025-06-14 15:20:35 +0300

    Add setters and clear method for BotRequestHolder

--- core/src/main/java/io/lonmstalker/core/storage/BotRequestHolder.java
+++ core/src/main/java/io/lonmstalker/core/storage/BotRequestHolder.java
@@ -10,6 +10,19 @@ public final class BotRequestHolder {
     private static final ThreadLocal<Update> UPDATE = new ThreadLocal<>();
     private static final ThreadLocal<TelegramSender> SENDER = new ThreadLocal<>();
 
+    public static void setUpdate(Update update) {
+        UPDATE.set(update);
+    }
+
+    public static void setSender(TelegramSender sender) {
+        SENDER.set(sender);
+    }
+
+    public static void clear() {
+        UPDATE.remove();
+        SENDER.remove();
+    }
+
     public static Update getUpdate() {
         return UPDATE.get();
     }
