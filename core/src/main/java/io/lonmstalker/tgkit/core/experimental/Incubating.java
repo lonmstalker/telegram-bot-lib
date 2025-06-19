715be040147d2178684d7806559e63b0db9a3fe4
Nikita <95490604+lonmstalker@users.noreply.github.com>
2025-06-19 23:59:12 +0300

    docs: translate Incubating javadoc (#76)

---
+++ core/src/main/java/io/lonmstalker/tgkit/core/experimental/Incubating.java
@@ -0,0 +1,30 @@
+package io.lonmstalker.tgkit.core.experimental;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Помечает API как экспериментальный.
+ *
+ * <p>Элементы, аннотированные {@code @Incubating}, могут измениться или быть удалены
+ * без предупреждения.
+ *
+ * <p>Пример использования:
+ *
+ * <pre>{@code
+ * @Incubating
+ * public class ExperimentalApi { }
+ *
+ * @Incubating
+ * public void experimentalMethod() { }
+ * }
+ * </pre>
+ */
+@Documented
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.TYPE, ElementType.METHOD})
+public @interface Incubating {
+}
