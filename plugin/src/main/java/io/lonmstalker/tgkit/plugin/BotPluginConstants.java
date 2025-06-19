45def448ec1ad972bd598212eb572994bfb8d803
Nikita <95490604+lonmstalker@users.noreply.github.com>
2025-06-20 00:48:14 +0300

    Translate Javadocs (#87)

--- plugin/src/main/java/io/lonmstalker/tgkit/plugin/BotPluginConstants.java
+++ plugin/src/main/java/io/lonmstalker/tgkit/plugin/BotPluginConstants.java
@@ -4,6 +4,18 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+/**
+ * Набор констант, используемых системой плагинов.
+ *
+ * <p>Пример использования:
+ * <pre>{@code
+ * if (descriptor.api() > BotPluginConstants.CURRENT_VERSION) {
+ *     throw new UnsupportedOperationException();
+ * }
+ * }
+ * </pre>
+ */
 public class BotPluginConstants {
+    /** Текущая поддерживаемая версия API плагинов. */
     public static final Double CURRENT_VERSION = 0.1;
 }
