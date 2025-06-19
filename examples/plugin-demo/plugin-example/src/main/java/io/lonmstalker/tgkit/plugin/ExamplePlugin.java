16724b1cbf8e89f3ff5686a5f1cb00916decc11d
Nikita <95490604+lonmstalker@users.noreply.github.com>
2025-06-20 01:55:51 +0300

    Replace print statements with SLF4J logger (#92)

--- examples/plugin-demo/plugin-example/src/main/java/io/lonmstalker/tgkit/plugin/ExamplePlugin.java
+++ examples/plugin-demo/plugin-example/src/main/java/io/lonmstalker/tgkit/plugin/ExamplePlugin.java
@@ -1,36 +1,40 @@
 package io.lonmstalker.tgkit.plugin;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ExamplePlugin implements BotPlugin {
 
+    private static final Logger log = LoggerFactory.getLogger(ExamplePlugin.class);
+
     @Override
     public void onLoad(@NonNull BotPluginContext ctx) {
-        System.out.println("[Example] onLoad");
+        log.info("onLoad");
     }
 
     @Override
     public void start() {
-        System.out.println("[Example] start");
+        log.info("start");
     }
 
     @Override
     public void beforeStop() {
-        System.out.println("[Example] beforeStop");
+        log.info("beforeStop");
     }
 
     @Override
     public void stop() {
-        System.out.println("[Example] stop");
+        log.info("stop");
     }
 
     @Override
     public void afterStop() {
-        System.out.println("[Example] afterStop");
+        log.info("afterStop");
     }
 
     @Override
     public void onUnload() {
-        System.out.println("[Example] onUnload");
+        log.info("onUnload");
     }
 }
