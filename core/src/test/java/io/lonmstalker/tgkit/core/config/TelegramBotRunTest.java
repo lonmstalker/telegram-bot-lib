2e03c95c7f94055e43af867888962e9c0b224953
Nikita <95490604+lonmstalker@users.noreply.github.com>
2025-06-20 15:23:49 +0300

    docs: add license headers

--- core/src/test/java/io/lonmstalker/tgkit/core/config/TelegramBotRunTest.java
+++ core/src/test/java/io/lonmstalker/tgkit/core/config/TelegramBotRunTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2025 TgKit Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.lonmstalker.tgkit.core.config;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,7 +41,9 @@ class TelegramBotRunTest {
       Path f = tmp.resolve("bot.yaml");
       String yaml =
           "token: T\n"
-              + "base-url: " + server.baseUrl() + "\n"
+              + "base-url: "
+              + server.baseUrl()
+              + "\n"
               + "bot-group: g\n"
               + "requests-per-second: 20\n"
               + "packages:\n  - io.test\n";
