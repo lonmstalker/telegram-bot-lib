fff574f03b3425bf82b1dbecdc798369bf0e5bd9
Nikita <95490604+lonmstalker@users.noreply.github.com>
2025-06-20 15:24:13 +0300

    docs: add license headers (#143)

--- core/src/main/java/io/lonmstalker/tgkit/core/TelegramBot.java
+++ core/src/main/java/io/lonmstalker/tgkit/core/TelegramBot.java
@@ -1,11 +1,26 @@
+/*
+ * Copyright 2025 TgKit Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.lonmstalker.tgkit.core;
 
 import io.lonmstalker.tgkit.core.bot.Bot;
 import io.lonmstalker.tgkit.core.bot.BotAdapter;
 import io.lonmstalker.tgkit.core.bot.BotAdapterImpl;
-import io.lonmstalker.tgkit.core.bot.TelegramSender;
 import io.lonmstalker.tgkit.core.bot.BotConfig;
 import io.lonmstalker.tgkit.core.bot.BotFactory;
+import io.lonmstalker.tgkit.core.bot.TelegramSender;
 import io.lonmstalker.tgkit.core.config.BotConfigLoader;
 import io.lonmstalker.tgkit.core.config.BotConfigLoader.Settings;
 import io.lonmstalker.tgkit.core.init.BotCoreInitializer;
@@ -13,9 +28,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
-/**
- * Вспомогательный класс для запуска бота по конфигурационному файлу.
- */
+/** Вспомогательный класс для запуска бота по конфигурационному файлу. */
 public final class TelegramBot {
 
   private TelegramBot() {}
