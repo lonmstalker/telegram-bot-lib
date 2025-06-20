98d6e279c4a7a9ca8318a9c674f0b30eb1a8a22d
Nikita <95490604+lonmstalker@users.noreply.github.com>
2025-06-20 21:05:56 +0300

    Remove wildcard imports

--- api/src/main/java/io/lonmstalker/tgkit/core/annotation/wizard/WizardMeta.java
+++ api/src/main/java/io/lonmstalker/tgkit/core/annotation/wizard/WizardMeta.java
@@ -15,7 +15,11 @@
  */
 package io.lonmstalker.tgkit.core.annotation.wizard;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Маркирует класс как wizard-сценарий.
