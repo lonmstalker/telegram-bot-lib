c2ad650be575e5e6442651e8b4ff907b150ff3f5
Nikita <95490604+lonmstalker@users.noreply.github.com>
2025-06-20 14:08:41 +0300

    Remove Lombok usage (#125)

--- core/src/main/java/io/lonmstalker/tgkit/core/bot/BotDataSourceConfig.java
+++ core/src/main/java/io/lonmstalker/tgkit/core/bot/BotDataSourceConfig.java
@@ -4,9 +4,6 @@ import javax.sql.DataSource;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
-@Getter
-@Builder
-@AllArgsConstructor
 public class BotDataSourceConfig {
   private @Nullable BotConfig botConfig;
   private @NonNull DataSource dataSource;
