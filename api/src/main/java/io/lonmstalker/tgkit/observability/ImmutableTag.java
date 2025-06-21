99efc2712036765f52762beb8678e5716452a53a
Nikita <95490604+lonmstalker@users.noreply.github.com>
2025-06-21 04:31:59 +0300

    docs: add Russian javadocs (#187)

--- api/src/main/java/io/lonmstalker/tgkit/observability/ImmutableTag.java
+++ api/src/main/java/io/lonmstalker/tgkit/observability/ImmutableTag.java
@@ -17,8 +17,15 @@ package io.lonmstalker.tgkit.observability;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
 
+/** Неизменяемый тег. */
 public record ImmutableTag(String key, String value) implements Tag {
 
+  /**
+   * Создаёт тег с ключом и значением.
+   *
+   * @param key ключ
+   * @param value значение
+   */
   public ImmutableTag(@NonNull String key, @NonNull String value) {
     this.key = key;
     this.value = value;
