a956c0c0d3fc1dd720a14d50d38a9e1ad5169a96
Nikita <95490604+lonmstalker@users.noreply.github.com>
2025-06-21 21:30:35 +0300

    Fix ChildFirstURLClassLoader package

--- plugin/src/main/java/io/github/tgkit/plugin/internal/ChildFirstURLClassLoader.java
+++ plugin/src/main/java/io/github/tgkit/plugin/internal/ChildFirstURLClassLoader.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.github.tgkit.plugin;
+package io.github.tgkit.plugin.internal;
 
 import java.net.URL;
 import java.net.URLClassLoader;
